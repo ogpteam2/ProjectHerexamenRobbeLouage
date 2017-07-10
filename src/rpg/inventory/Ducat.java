bbcff9924abd24c9a8b1a8d956e4bb3c5c50a9f2
Robbe <Robbe@DESKTOP-H7A6K9O>
2017-07-11 01:09:39 +0200

    Ducat

--- src/rpg/inventory/Ducat.java
+++ src/rpg/inventory/Ducat.java
@@ -1,6 +1,13 @@
 package rpg.inventory;
 import be.kuleuven.cs.som.annotate.*;
 
-public class Ducat extends Item {
+/**
+ * A class of Ducat, the unit of money in the rpg.
+ * 
+ * @author Robbe
+ * @version 1.0
+ */
+public class Ducat{
+
 
 }
