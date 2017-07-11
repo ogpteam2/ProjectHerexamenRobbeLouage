7b1198ec9ae57b2e932501fd61f4c14ac813423a
Robbe <Robbe@DESKTOP-H7A6K9O>
2017-07-11 16:16:56 +0200

    Name completed

--- src/rpg/Monster.java
+++ src/rpg/Monster.java
@@ -1,5 +1,7 @@
 package rpg;
 
+import java.util.regex.Pattern;
+
 import be.kuleuven.cs.som.annotate.*;
 
 /**
@@ -20,7 +22,7 @@ public class Monster extends Mobile {
 	 * @param name
 	 *        The name of the monster.
 	 */
-	protected Monster(String name) {
+	public Monster(String name) {
 		super(name);
 
 	}
@@ -29,10 +31,39 @@ public class Monster extends Mobile {
 	 * Name: defensive
 	 ************************************************/
 	
+	/**
+	 * Check whether the given name is a valid one.
+	 * 
+	 * @return True if and only if the name is effective and it is a valid monsterName
+	 * 	       | result == 
+	 * 		   | 	( (name != null) &&
+	 *         | 	  (isValidMonsterName(name) )
+	 */
+	@Override
+	public boolean isValidName(String name){
+		 return super.isValidName(name) && isValidMonsterName(name);
+	}
 	
 	
+	/**
+	 * 
+	 * Checks whether the name is a valid monster name
+	 * 
+	 * @param name
+	 * 	      The name to check.
+	 * @return true if the name matches with the pattern.
+	 * 		   | result == name.matches("[A-Z]['A-Za-z\\s]+")
+	 */
+	private boolean isValidMonsterName(String name){
+		return firstPattern.matcher(name).matches();
+	}
 	
-	
+	/**
+	 * The  pattern to check the name against, checks if the name
+	 * starts with a capital and consists of only ',letters and spaces.
+	 */
+	@Model
+	private static final Pattern firstPattern = Pattern.compile("[A-Z]['A-Za-z\\s]+");
 	
 	
 	
--- src/rpg/testcases/MonsterTest.java
+++ src/rpg/testcases/MonsterTest.java
@@ -2,13 +2,44 @@ package rpg.testcases;
 
 import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import rpg.Hero;
+import rpg.Monster;
 
 public class MonsterTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private Monster invalidName1,invalidName2,invalidName3,validName1,validName2,validName3;
+	
+	@Before
+	public void setup(){
+		
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void isValidName1() {
+		invalidName1 = new Monster(null);
+	}
+	@Test (expected = IllegalArgumentException.class)
+	public void isValidName2() {
+		invalidName2 = new Monster("bob");
+	}
+	@Test (expected = IllegalArgumentException.class)
+	public void isValidName3() {
+		invalidName3 = new Monster("b*b");
+	}
+	@Test 
+	public void isValidName4() {
+		validName1 = new Monster("James");
+	}
+	@Test 
+	public void isValidName5() {
+		validName2 = new Monster("H'erld");
+	}
+	@Test 
+	public void isValidName6() {
+		validName3 = new Monster("Blub");
 	}
 
 }
